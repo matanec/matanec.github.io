7e937d0669f5973dbeeea1a262c4c2e15d8dc8a1
Sergejs Matancevs <matanec@hotmail.com>
2017-07-01 20:47:15 +0300

    deep added

--- js/temp_script.js
+++ js/temp_script.js
@@ -3,9 +3,6 @@ $(function(){
     // Instantiate Bootstrap tooltip
     $('[data-toggle="tooltip"]').tooltip(); 
 
-    //monitor main menu position
-    //$("#topmenu").sticky({ topSpacing: 0 });
-
     //yamm 
     $(document).on('click', '.yamm .dropdown-menu', function(e) {
         e.stopPropagation()
